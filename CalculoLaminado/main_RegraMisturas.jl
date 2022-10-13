aca3724d50d666b9c88d8204c09f989a5fdaa31b
Verônica Pazda <107930972+carolpazda@users.noreply.github.com>
2022-10-13 14:19:30 -0300

    Update main_RegraMisturas.jl

--- CalculoLaminado/main_RegraMisturas.jl
+++ CalculoLaminado/main_RegraMisturas.jl
@@ -67,51 +67,50 @@ function main()
 
     # Carregamento do Laminado? [N e Nm]
     Nx = 1000000.0
-	Ny = 200000.0
-	Nxy = 0.0 
-	Mx = 0.0
-	My = 0.0
-	Mxy = 0.0
+    Ny = 200000.0
+    Nxy = 0.0 
+    Mx = 0.0
+    My = 0.0
+    Mxy = 0.0
 
     # De forma que o vetor de carregamento vai ser dado por
-	N =[Nx;Ny;Nxy;Mx;My;Mxy] 
+    N =[Nx;Ny;Nxy;Mx;My;Mxy] 
 
     # Propriedades de resistência do material -> Todas em Pa!!!
-	Xt = 1447.0E6      # -> Resistência à tração 
-	Xc = -1447.0E6     # -> Resistência à compressão 
-	Yt = 51.7E6        # -> Resistência à tração 
-	Yc = -206.0E6      # -> Resistência à compressão 
-	S12 = 93.0E6       # -> Resistência ao cisalhamento no plano 1-2 
+    Xt = 1447.0E6      # -> Resistência à tração 
+    Xc = -1447.0E6     # -> Resistência à compressão 
+    Yt = 51.7E6        # -> Resistência à tração 
+    Yc = -206.0E6      # -> Resistência à compressão 
+    S12 = 93.0E6       # -> Resistência ao cisalhamento no plano 1-2 
 
     # Calculamos a matriz de rigidez do nosso problema em questão
     Q = matriz_Q(E11,E22,G12,v12)
 
     # Calculamos o h e o zm
-	#h,zm =  planoMedio(espessura,n_camadas) -> Não estou precisando utilizar
+    #h,zm =  planoMedio(espessura,n_camadas) -> Não estou precisando utilizar
 	
     # Calculamos a matriz ABBD
-	ABBD= ABBD_laminado(v,n_camadas,E11, E22, G12, v12, angulo) # -> Verificado
+    ABBD= ABBD_laminado(v,n_camadas,E11, E22, G12, v12, angulo) # -> Verificado
 
     # Deformação no plano médio do laminado
-	Defo_PlanoMedio = ABBD\N # Verificado
+    Defo_PlanoMedio = ABBD\N # Verificado
 
-	# Calcula as Tensões e Deformações globais # -> Verificado também
-	tensao_global, tensao_local, deformacao_global, deformacao_local = Defor_Tensoes(E11, E22, G12, v12, 
+    # Calcula as Tensões e Deformações globais # -> Verificado também
+    tensao_global, tensao_local, deformacao_global, deformacao_local = Defor_Tensoes(E11, E22, G12, v12, 
 	                                                n_camadas, v, angulo, Defo_PlanoMedio[1:3], Defo_PlanoMedio[4:6])
 
 	
-	# CÁLCULO DOS CRITÉRIOS DE FALHA
-	# 1 -> Máxima Tensão
-	MaxTensao = MaximaTensao(n_camadas, tensao_local, Xt, Xc, Yt, Yc, S12)
+    # CÁLCULO DOS CRITÉRIOS DE FALHA
+    # 1 -> Máxima Tensão
+    MaxTensao = MaximaTensao(n_camadas, tensao_local, Xt, Xc, Yt, Yc, S12)
 
-	# 2 -> Tsai-Hill
-	TsaiHill = Tsai_Hill(n_camadas, tensao_local, Xt, Xc, Yt, Yc, S12)
+    # 2 -> Tsai-Hill
+    TsaiHill = Tsai_Hill(n_camadas, tensao_local, Xt, Xc, Yt, Yc, S12)
 
-	# 3 -> Tsai-Wu
-	TsaiWu = Wu(Xt, Xc, Yt, Yc, S12, n_camadas, tensao_local)
+    # 3 -> Tsai-Wu
+    TsaiWu = Wu(Xt, Xc, Yt, Yc, S12, n_camadas, tensao_local)
 
-	# Vamos retornar tudo que é de interesse para o usuário
-	return ABBD, Defo_PlanoMedio, tensao_global, tensao_local, deformacao_global, 
-	       deformacao_local, MaxTensao, TsaiHill, TsaiWu
+    # Vamos retornar tudo que é de interesse para o usuário
+    return ABBD, Defo_PlanoMedio, tensao_global, tensao_local, deformacao_global, deformacao_local, MaxTensao, TsaiHill, TsaiWu
 
 end
